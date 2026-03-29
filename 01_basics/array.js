37a372b60c1fcf0a3d88bc7cbed109ff150bbf4d
Muhammad Hadi <muhammadhadiofficial123@gmail.com>
2026-03-29 19:10:20 +0500

    Moved Files in folder

---
+++ 01_basics/array.js
@@ -0,0 +1,29 @@
+const marvel_heroes = ["thor", "spiderman", "Ironman"]
+const dc_heroes = ["superman", "batman", "flash"]
+
+// marvel_heroes.push(dc_heroes)
+
+// concate 2 or more than 2 arrays in one new array.
+// const all_heroes = marvel_heroes.concat(dc_heroes)
+// console.log(all_heroes);
+
+// Spread concept
+// spreading each array and merge them in one.
+// const all_new_heroes = [...marvel_heroes, ...dc_heroes]
+// console.log(all_new_heroes);
+
+// Complex situations of array
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+console.log(Array.isArray("Hadi"))
+console.log(Array.from("Hadi"))
+console.log(Array.from({name: "hadi"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
---
+++ 01_basics/arrays.js
@@ -0,0 +1,50 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+// console.log(myArr[1]);
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr2[1]);
+
+// Arrays Methods
+
+// push() an element in the end position of array.
+// myArr.push(6)
+// myArr.push(7)
+
+// pop() an element from the end position of array.
+// myArr.pop()
+
+// unshift() means to add up an element in the first position of array.
+// myArr.unshift(9)
+
+// shift() means to remove an element from the first position of array.
+// myArr.shift()
+
+// Checks the element in the array that it is includes or not (true or false answer).
+// console.log(myArr.includes(9));
+
+/* Tells that the element in the array is occuring if the element is 
+not in the array then it gives -1 value which is false.*/
+// console.log(myArr.indexOf(3));
+
+// Converts the array into string.
+// const newArr = myArr.join()
+
+// console.log(newArr);
+// console.log(myArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
