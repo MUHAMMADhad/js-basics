468bc1a74b4218cfdc74ac7cf28545e10fc6a13d
Muhammad Hadi <muhammadhadiofficial123@gmail.com>
2026-03-29 21:20:16 +0500

    Updated!

--- 01_basics/array.js
+++ 01_basics/array.js
@@ -1,5 +1,5 @@
-const marvel_heroes = ["thor", "spiderman", "Ironman"]
-const dc_heroes = ["superman", "batman", "flash"]
+// const marvel_heroes = ["thor", "spiderman", "Ironman"]
+// const dc_heroes = ["superman", "batman", "flash"]
 
 // marvel_heroes.push(dc_heroes)
 
@@ -13,17 +13,17 @@ const dc_heroes = ["superman", "batman", "flash"]
 // console.log(all_new_heroes);
 
 // Complex situations of array
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 // const real_another_array = another_array.flat(Infinity)
 // console.log(real_another_array);
 
-console.log(Array.isArray("Hadi"))
-console.log(Array.from("Hadi"))
-console.log(Array.from({name: "hadi"})) // interesting
+// console.log(Array.isArray("Hadi"))
+// console.log(Array.from("Hadi"))
+// console.log(Array.from({name: "hadi"})) // interesting
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+// console.log(Array.of(score1, score2, score3));
